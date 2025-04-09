9fd16a0912ad7001fdcd63094f1b8c439a82f46f
FarVillage117 <143549383+FarVillage117@users.noreply.github.com>
2025-04-09 11:49:29 -0400

    7.3 Additions

--- 314-ASMT-W8-Unity/Assets/Scripts/System/XrAudioManager.cs
+++ 314-ASMT-W8-Unity/Assets/Scripts/System/XrAudioManager.cs
@@ -2,45 +2,68 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR.Interaction.Toolkit;
 public class XrAudioManager : MonoBehaviour
 {
-    
+    [SerializeField] XRGrabInteractable[] grabInteractables;
+    [SerializeField] AudioSource grabSound;
+    [SerializeField] AudioClip grabClip;
+    [SerializeField] AudioClip keyClip;
+    [SerializeField] AudioSource activatedSound;
+    [SerializeField] AudioClip grabActivatedClip;
+    [SerializeField] AudioClip wandActivatedClip;
+    [SerializeField] TheWall wall;
     [SerializeField] AudioSource wallSource;
     [SerializeField] AudioClip destroyWallClip;
-    [SerializeField] private AudioClip fallBackClip;
-    private const string FallBackClip_Name = "fallBackClip";
 
     private void OnEnable()
     {
+        grabInteractables = FindObjectsByType<XRGrabInteractable>(FindObjectsSortMode.None);
+        for (int i = 0; i < grabInteractables.Length; i++)
+        {
+            grabInteractables[i].selectEntered.AddListener(OnSelectEnterGrabbable);
+            grabInteractables[i].selectExited.AddListener(OnSelectExitGrabbable);
+            grabInteractables[i].activated.AddListener(OnActivatedGrabbable);
+        }
         if (fallBackClip == null)
         {
             fallBackClip = AudioClip.Create(FallBackClip_Name, 1, 1, 1000, true);
         }
-        if (wall != null)
+    }
+
+    private void OnActivatedGrabbable(ActivateEventArgs arg0)
+    {
+        GameObject tempGameObject = arg0.interactableObject.transform.gameObject;
+        if (tempGameObject.GetComponent<WandControl>() != null)
         {
-            destroyWallClip = wall.GetDestroyClip;
-            if (destroyWallClip == null)
-            {
-                destroyWallClip = fallBackClip;
-            }
-            wall.OnDestroy.AddListener(OnDestroyWall);
+            activatedSound.clip = wandActivatedClip;
         }
+        else
+        {
+            activatedSound.clip = grabActivatedClip;
+        }
+        activatedSound.Play();
+    }
+
+    private void OnSelectExitGrabbable(SelectExitEventArgs arg0)
+    {
+        grabSound.clip = grabClip;
+        grabSound.Play();
+    }
+
+    private void OnSelectEnterGrabbable(SelectEnterEventArgs arg0)
+    {
+        if (arg0.interactableObject.transform.CompareTag("Key"))
+        {
+            grabSound.clip = keyClip;
+        }
+        else
+        {
+            grabSound.clip = grabClip;
+        }
+        grabSound.Play();
     }
 
     private void OnDestroyWall()
     {
         if (wallSource != null)
-        {
-            wallSource.Play();
-        }
-    }
-
-    private void OnDisable()
-    {
-        if (wall != null)
-        {
-            wall.OnDestroy.RemoveListener(OnDestroyWall);
-        }
-    }
-}
